499a31bd27bcbd335d9fb51b67f667baa0ed6207
Hikaru <hikaru.purba@gmail.com>
2020-02-26 23:56:13 -0700

    Finished Server.java file, next is all the handlers

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -24,11 +24,14 @@ import model.User;
 */
 public class Server {
 
+    /*
     // The maximum number of waiting incoming connections to queue.
     // While this value is necessary, for our purposes it is unimportant.
     // Take CS 460 for a deeper understanding of what it means.
+     */
     private static final int MAX_WAITING_CONNECTIONS = 12;
 
+    /*
     // Java provides an HttpServer class that can be used to embed
     // an HTTP server in any Java program.
     // Using the HttpServer class, you can easily make a Java
@@ -38,25 +41,30 @@ public class Server {
     // protocol (be glad you don't have to).
     // The "server" field contains the HttpServer instance for this program,
     // which is initialized in the "run" method below.
+     */
     private HttpServer server;
 
+    /*
     // This method initializes and runs the server.
     // The "portNumber" parameter specifies the port number on which the
     // server should accept incoming client connections.
+     */
     private void run(String portNumber) {
-
+        /*
         // Since the server has no "user interface", it should display "log"
         // messages containing information about its internal activities.
         // This allows a system administrator (or you) to know what is happening
         // inside the server, which can be useful for diagnosing problems
         // that may occur.
+         */
         System.out.println("Initializing HTTP Server");
-
         try {
+            /*
             // Create a new HttpServer object.
             // Rather than calling "new" directly, we instead create
             // the object by calling the HttpServer.create static factory method.
             // Just like "new", this method returns a reference to the new object.
+             */
             server = HttpServer.create(
                     new InetSocketAddress(Integer.parseInt(portNumber)),
                     MAX_WAITING_CONNECTIONS);
@@ -65,74 +73,61 @@ public class Server {
             e.printStackTrace();
             return;
         }
-
+        /*
         // Indicate that we are using the default "executor".
         // This line is necessary, but its function is unimportant for our purposes.
+         */
         server.setExecutor(null);
-
+        /*
         // Log message indicating that the server is creating and installing
         // its HTTP handlers.
         // The HttpServer class listens for incoming HTTP requests.  When one
         // is received, it looks at the URL path inside the HTTP request, and
         // forwards the request to the handler for that URL path.
+         */
         System.out.println("Creating contexts");
-
-        // Create and install the HTTP handler for the "/games/list" URL path.
+        /*
+        // Create and install the HTTP handler for the "/_/_ " URL path.
         // When the HttpServer receives an HTTP request containing the
-        // "/games/list" URL path, it will forward the request to ListGamesHandler
-        // for processing.
-        // server.createContext("/games/list", new ListGamesHandler());
+        // "/_/_ " URL path, it will forward the request to __Handler for processing.
+         */
         registerHandlerFiles();
-
-        // Create and install the HTTP handler for the "/routes/claim" URL path.
-        // When the HttpServer receives an HTTP request containing the
-        // "/routes/claim" URL path, it will forward the request to ClaimRouteHandler
-        // for processing.
-
-//        server.createContext("/routes/claim", new ClaimRouteHandler());
-
-        // Log message indicating that the HttpServer is about the start accepting
-        // incoming client connections.
         System.out.println("Starting server");
-
+        /*
         // Tells the HttpServer to start accepting incoming client connections.
         // This method call will return immediately, and the "main" method
         // for the program will also complete.
         // Even though the "main" method has completed, the program will continue
         // running because the HttpServer object we created is still running
         // in the background.
+         */
         server.start();
-
         // Log message indicating that the server has successfully started.
         System.out.println("Server started");
     }
 
-    // "main" method for the server program
-    // "args" should contain one command-line argument, which is the port number
-    // on which the server should accept incoming client connections.
+    /**
+     * "main" method for the server program
+     * "args" should contain one command-line argument, which is the port number on which the server should
+     * accept incoming client connections.
+     * @param args
+     */
     public static void main(String[] args) {
-        String portNumber = args[0];    // port 8000
+        String portNumber = args[0];    // port 8000, or 8080
         new Server().run(portNumber);
     }
 
-
-
-    // HIKARU
     // Register Handler Files
-    public void registerHandlerFiles(){
+    public void registerHandlerFiles() {
         server.createContext("/", new DefaultHandler());                    // Default Handler
-        server.createContext("/user/register", new RegisterHandler());      // Creates a new user account, generates 4 generations of ancestor data for the new user, logs the user in, and returns an auth token.
-        server.createContext("/user/login", new LoginHandler());            // Logs in the user and returns an auth token
-        server.createContext("/clear", new ClearHandler());                 // Deletes ALL data from the database, including user accounts, auth tokens, and generated person and event data.
-        server.createContext("/fill", new FillHandler());                   // /fill/[username]/{generations} // Populates the server's database with generated data for the specified user name.
-//        The required "username" parameter must be a user already registered with the server. If there is
-//        any data in the database already associated with the given user name, it is deleted. The
-//        optional “generations” parameter lets the caller specify the number of generations of ancestors
-//        to be generated, and must be a non-negative integer (the default is 4, which results in 31 new
-//        persons each with associated events).
-        server.createContext("/load", new LoadHandler());                   // Clears all data from the database (just like the /clear API), and then loads the posted user, person, and event data into the database.
-        server.createContext("/person/", new PersonIDHandler());            // /person/[personID] // Returns the single Person object with the specified ID
-        server.createContext("/person", new PersonHandler());               //  Returns ALL family members of the current user. The current user is determined from the provided auth token
-        server.createContext("/event/", new EventIDHandler());              // /event/[eventID] //  Returns the single Event object with the specified ID.
-        server.createContext("/event", new EventHandler());                 // Returns ALL events for ALL family members of the current user. The current user is determined from the provided auth token.
+        // server.createContext("/user/register", new RegisterHandler());      // Creates a new user account, generates 4 generations of ancestor data for the new user, logs the user in, and returns an auth token.
+        // server.createContext("/user/login", new LoginHandler());            // Logs in the user and returns an auth token
+        // server.createContext("/clear", new ClearHandler());                 // Deletes ALL data from the database, including user accounts, auth tokens, and generated person and event data.
+        // server.createContext("/fill", new FillHandler());                   // /fill/[username]/{generations} // Populates the server's database with generated data for the specified user name.
+        // server.createContext("/load", new LoadHandler());                   // Clears all data from the database (just like the /clear API), and then loads the posted user, person, and event data into the database.
+        // server.createContext("/person/", new PersonIDHandler());            // /person/[personID] // Returns the single Person object with the specified ID
+        // server.createContext("/person", new PersonHandler());               //  Returns ALL family members of the current user. The current user is determined from the provided auth token
+        // server.createContext("/event/", new EventIDHandler());              // /event/[eventID] //  Returns the single Event object with the specified ID.
+        // server.createContext("/event", new EventHandler());                 // Returns ALL events for ALL family members of the current user. The current user is determined from the provided auth token.
+    }
 }
--- src/main/java/handler/LoginHandler.java
+++ src/main/java/handler/LoginHandler.java
@@ -10,73 +10,73 @@ public class LoginHandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
-        boolean success = false;
-
-        try {
-            // Determine the HTTP request type (GET, POST, etc.).
-            // Only allow GET requests for this operation.
-            // This operation requires a GET request, because the
-            // client is "getting" information from the server, and
-            // the operation is "read only" (i.e., does not modify the
-            // state of the server).
-            if (exchange.getRequestMethod().toLowerCase().equals("get")) {
-
-                // Get the HTTP request headers
-                Headers reqHeaders = exchange.getRequestHeaders();
-                // Check to see if an "Authorization" header is present
-                if (reqHeaders.containsKey("Authorization")) {
-
-                    // Extract the auth token from the "Authorization" header
-                    String authToken = reqHeaders.getFirst("Authorization");
-                    // Verify that the auth token is the one we're looking for
-                    // (this is not realistic, because clients will use different
-                    // auth tokens over time, not the same one all the time).
-                    if (authToken.equals("afj232hj2332")) {
-
-                        // This is the JSON data we will return in the HTTP response body
-                        // (this is unrealistic because it always returns the same answer).
-                        String respData =
-                                "{ \"game-list\": [" +
-                                        "{ \"name\": \"fhe game\", \"player-count\": 3 }," +
-                                        "{ \"name\": \"work game\", \"player-count\": 4 }," +
-                                        "{ \"name\": \"church game\", \"player-count\": 2 }" +
-                                        "]" +
-                                        "}";
-
-                        // Start sending the HTTP response to the client, starting with
-                        // the status code and any defined headers.
-                        exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0);
-
-                        // Now that the status code and headers have been sent to the client,
-                        // next we send the JSON data in the HTTP response body.
-
-                        // Get the response body output stream.
-                        OutputStream respBody = exchange.getResponseBody();
-                        // Write the JSON string to the output stream.
-                        writeString(respData, respBody);
-                        // Close the output stream.  This is how Java knows we are done
-                        // sending data and the response is complete/
-                        respBody.close();
-
-                        success = true;
-                    }
-                }
-            }
-
-            if (!success) {
-                // The HTTP request was invalid somehow, so we return a "bad request"
-                // status code to the client.
-                exchange.sendResponseHeaders(HttpURLConnection.HTTP_BAD_REQUEST, 0);
-                // Since the client request was invalid, they will not receive the
-                // list of games, so we close the response body output stream,
-                // indicating that the response is complete.
-                exchange.getResponseBody().close();
-            }
-        }
-        catch (IOException e) {
-            exchange.sendResponseHeaders(HttpURLConnection.HTTP_SERVER_ERROR, 0);
-            exchange.getResponseBody().close();
-            e.printStackTrace();
-        }
+//        boolean success = false;
+//
+//        try {
+//            // Determine the HTTP request type (GET, POST, etc.).
+//            // Only allow GET requests for this operation.
+//            // This operation requires a GET request, because the
+//            // client is "getting" information from the server, and
+//            // the operation is "read only" (i.e., does not modify the
+//            // state of the server).
+//            if (exchange.getRequestMethod().toLowerCase().equals("get")) {
+//
+//                // Get the HTTP request headers
+//                Headers reqHeaders = exchange.getRequestHeaders();
+//                // Check to see if an "Authorization" header is present
+//                if (reqHeaders.containsKey("Authorization")) {
+//
+//                    // Extract the auth token from the "Authorization" header
+//                    String authToken = reqHeaders.getFirst("Authorization");
+//                    // Verify that the auth token is the one we're looking for
+//                    // (this is not realistic, because clients will use different
+//                    // auth tokens over time, not the same one all the time).
+//                    if (authToken.equals("afj232hj2332")) {
+//
+//                        // This is the JSON data we will return in the HTTP response body
+//                        // (this is unrealistic because it always returns the same answer).
+//                        String respData =
+//                                "{ \"game-list\": [" +
+//                                        "{ \"name\": \"fhe game\", \"player-count\": 3 }," +
+//                                        "{ \"name\": \"work game\", \"player-count\": 4 }," +
+//                                        "{ \"name\": \"church game\", \"player-count\": 2 }" +
+//                                        "]" +
+//                                        "}";
+//
+//                        // Start sending the HTTP response to the client, starting with
+//                        // the status code and any defined headers.
+//                        exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0);
+//
+//                        // Now that the status code and headers have been sent to the client,
+//                        // next we send the JSON data in the HTTP response body.
+//
+//                        // Get the response body output stream.
+//                        OutputStream respBody = exchange.getResponseBody();
+//                        // Write the JSON string to the output stream.
+//                        writeString(respData, respBody);
+//                        // Close the output stream.  This is how Java knows we are done
+//                        // sending data and the response is complete/
+//                        respBody.close();
+//
+//                        success = true;
+//                    }
+//                }
+//            }
+//
+//            if (!success) {
+//                // The HTTP request was invalid somehow, so we return a "bad request"
+//                // status code to the client.
+//                exchange.sendResponseHeaders(HttpURLConnection.HTTP_BAD_REQUEST, 0);
+//                // Since the client request was invalid, they will not receive the
+//                // list of games, so we close the response body output stream,
+//                // indicating that the response is complete.
+//                exchange.getResponseBody().close();
+//            }
+//        }
+//        catch (IOException e) {
+//            exchange.sendResponseHeaders(HttpURLConnection.HTTP_SERVER_ERROR, 0);
+//            exchange.getResponseBody().close();
+//            e.printStackTrace();
+//        }
     }
 }
